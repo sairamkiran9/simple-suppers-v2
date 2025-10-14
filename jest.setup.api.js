118d7a3beaa06b31084964501a7ed4019fb8eed6
sairamkiran9 <sairamkiranmuppana.1999@gmail.com>
2025-10-14 14:23:47 -0400

    Update: Remove mock secrets from jest config

--- jest.setup.api.js
+++ jest.setup.api.js
@@ -1,11 +1,6 @@
 // Jest setup for API tests (Node environment)
 require('jest-extended')
 
-// Mock environment variables
-process.env.NEXT_PUBLIC_SUPABASE_URL = 'http://localhost:54321'
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'test-anon-key'
-process.env.SUPABASE_SERVICE_ROLE_KEY = 'test-service-role-key'
-
 // Add globals for Node environment
 const { TextEncoder, TextDecoder } = require('util')
 global.TextEncoder = TextEncoder
--- jest.setup.database.js
+++ jest.setup.database.js
@@ -1,19 +1,6 @@
 // Jest setup for database tests (Node environment with real Supabase connection)
 require('jest-extended')
 
-// Set default environment variables for database tests if not provided
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  process.env.NEXT_PUBLIC_SUPABASE_URL = 'http://localhost:54321'
-}
-
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
-}
-
-if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-  process.env.SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'
-}
-
 // Add globals for Node environment
 const { TextEncoder, TextDecoder } = require('util')
 global.TextEncoder = TextEncoder

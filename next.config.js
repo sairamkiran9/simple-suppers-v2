32a8a93919186f8438c31a2aca0518c7e114094c
sairamkiran9 <sairamkiranmuppana.1999@gmail.com>
2025-11-26 16:20:02 -0500

    Chore: Optimise bundle size - remove unwanted files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,4 +10,4 @@ const nextConfig = {
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
